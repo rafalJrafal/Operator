374fbabc0d84978411703e8a260f55883c8e0f4b
Raf <rafal.jakuta@gmail.com>
2019-08-08 16:20:02 +0200

    new operators added

--- inc/ClassX.h
+++ inc/ClassX.h
@@ -17,11 +17,25 @@ class ClassX : public BaseClass {
 			log->log("Destructor ClassX this - %p", this);
 		}
 		const ClassX & operator = (const int & value) {
-			log->log("Operator = (int) value = %d", value);
+			log->log("Operator = (int) for %p value = %d", this, value);
 			mValue = value;
 			return *this;
 		}
-		
+		const ClassX & operator = (const ClassX & value) {
+			log->log("Operator = (ClassX) for %p value = %d from %p", this, value.mValue, &value);
+			mValue = value.mValue;
+			return *this;
+		}
+		const ClassX & operator ++ (int) {
+			log->log("Operator ClassX++ for %p value = %d", this, mValue);
+			this->mValue++;
+			return *this;
+		}
+		const ClassX & operator ++ () {
+			log->log("Operator ++ClassX for %p value = %d", this, mValue);
+			this->mValue++;
+			return *this;
+		}
 	private:
 		int mValue;
 	public:
