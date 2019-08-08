ee00c92d05c506c166b36bca7de10dbd56787577
Raf <rafal.jakuta@gmail.com>
2019-08-08 14:20:15 +0200

    logging added

--- inc/BaseClass.h
+++ inc/BaseClass.h
@@ -3,8 +3,24 @@
 
 #include "../../LogSystem/inc/LogSystem.h"
 
+namespace OperatorTest {
+
 class BaseClass {
-	
+	public:
+		BaseClass() {
+			log = LogSystem::LogSystem::instance("operator.txt");
+			log->log("Constructor BaseClass this - %p", this);
+		}
+		~BaseClass() {
+			log->log("Destructor BaseClass this - %p", this);
+		}
+		BaseClass(const BaseClass& copy) {
+			log->log("Copy Constructor BaseClass this - %p copy from %d", this, &copy);
+		}
+	protected:
+		LogSystem::LogSystem * log;
 };
+
+}
 
 #endif
--- inc/ClassX.h
+++ inc/ClassX.h
@@ -3,8 +3,36 @@
 
 #include "BaseClass.h"
 
+namespace OperatorTest {
+
 class ClassX : public BaseClass {
-	
+	public:
+		ClassX() {
+			log->log("Constructor ClassX this - %p", this);
+		}
+		ClassX(const ClassX & copy) {
+			log->log("Copy Constructor ClassX this - %p, copy from - %p", this, &copy);
+		}
+		~ClassX() {
+			log->log("Destructor ClassX this - %p", this);
+		}
+		const ClassX & operator = (const int & value) {
+			log->log("Operator = (int) value = %d", value);
+			mValue = value;
+			return *this;
+		}
+		
+	private:
+		int mValue;
+	public:
+		void set(int value) {
+			mValue = value;
+		}
+		int get() {
+			return mValue;
+		}
 };
+
+}
 
 #endif
