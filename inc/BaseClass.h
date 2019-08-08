4039c8757b3ec149c05d31f0b55c1fba0f4dce91
Raf <rafal.jakuta@gmail.com>
2019-08-08 11:55:02 +0200

    initial commit

---
+++ inc/BaseClass.h
@@ -0,0 +1,10 @@
+#ifndef BASECLASS_H
+#define BASECLASS_H
+
+#include "../../LogSystem/inc/LogSystem.h"
+
+class BaseClass {
+	
+};
+
+#endif
---
+++ inc/ClassX.h
@@ -0,0 +1,10 @@
+#ifndef CLASSX_H
+#define CLASSX_H
+
+#include "BaseClass.h"
+
+class ClassX : public BaseClass {
+	
+};
+
+#endif
