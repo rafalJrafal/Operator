d51584863b81b6f8387de825ae527794753ce931
Raf <rafal.jakuta@gmail.com>
2019-08-20 16:53:22 +0200

    removed new / del operators

--- inc/ClassX.h
+++ inc/ClassX.h
@@ -42,9 +42,15 @@ class ClassX : public BaseClass {
 			this->mValue += value.mValue;
 			return *this;
 		}
-		void * operator new (size_t size) {
+/*		void * operator new (size_t size) {
 			static LogSystem::LogSystem * sLog = LogSystem::LogSystem::instance(OPERATOR_LOG_FILENAME);
 			ClassX * p = ::new ClassX();
+			sLog->log("Operator new ClassX %p size = %d.", p, size);
+			return p;
+		}
+		void * operator new (size_t size, const char * file, int line) {
+			static LogSystem::LogSystem * sLog = LogSystem::LogSystem::instance(OPERATOR_LOG_FILENAME);
+			ClassX * p = new(file, line) ClassX();
 			sLog->log("Operator new ClassX %p size = %d.", p, size);
 			return p;
 		}
@@ -52,7 +58,19 @@ class ClassX : public BaseClass {
 			static LogSystem::LogSystem * sLog = LogSystem::LogSystem::instance(OPERATOR_LOG_FILENAME);
 			sLog->log("Operator new ClassX size = %d, num alloc = %d", size, numAllocations);
 			for (int i = 0; i < numAllocations; i++) {
-				ClassX * p = new ClassX;
+				ClassX * p = ::new ClassX;
+				sLog->log("Operator new ClassX %p tmpAlloc = %d", p, i);
+				delete p;				
+			}
+			ClassX * p = ::new ClassX();
+			sLog->log("Operator new ClassX %p size = %d.", p, size);
+			return p;
+		}
+		void * operator new (size_t size, unsigned int numAllocations, const char * file, int line) {
+			static LogSystem::LogSystem * sLog = LogSystem::LogSystem::instance(OPERATOR_LOG_FILENAME);
+			sLog->log("Operator new ClassX size = %d, num alloc = %d", size, numAllocations);
+			for (int i = 0; i < numAllocations; i++) {
+				ClassX * p = ::new ClassX;
 				sLog->log("Operator new ClassX %p tmpAlloc = %d", p, i);
 				delete p;				
 			}
@@ -68,6 +86,14 @@ class ClassX : public BaseClass {
 				p, size, sizeof(ClassX), numberOfElements);
 			return p;
 		}
+		void * operator new [] (size_t size, const char * file, int line) {
+			static LogSystem::LogSystem * sLog = LogSystem::LogSystem::instance(OPERATOR_LOG_FILENAME);
+			const unsigned int numberOfElements = size/sizeof(ClassX);
+			ClassX * p = new (file, line) ClassX[numberOfElements];
+			sLog->log("Operator new ClassX %p size = %d sizefo(ClassX) = %d, numberOfElements = %d.", 
+				p, size, sizeof(ClassX), numberOfElements);
+			return p;
+		}
 		void operator delete (void * p) {
 			static LogSystem::LogSystem * sLog = LogSystem::LogSystem::instance(OPERATOR_LOG_FILENAME);
 			sLog->log("Operator delete ClassX %p", p);
@@ -79,7 +105,7 @@ class ClassX : public BaseClass {
 			sLog->log("Operator delete [] ClassX %p", p);
 			ClassX * x_p = (ClassX*)p;
 			::delete [] x_p;
-		}
+		} */
 	private:
 		int mValue;
 	public:
